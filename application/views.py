0c67096fe38e8ae23d3ddec0b1cc0d80170f4a28
kevinchege47 <chegekevin001@gmail.com>
2023-01-29 19:38:37 +0300

    views and urls setup

--- application/views.py
+++ application/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def gallery(request):
+    return render(request,'application/gallery.html')
+
+def viewPhoto(request):
+    return render(request,'application/photo.html')
+
+def addPhoto(request):
+    return render(request,'application/add.html')
--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@an8(ni3fof!(mlet@vy$)&2psbvbr!ib@qw1^w71muy51mgi)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'application.apps.ApplicationConfig'
 ]
 
 MIDDLEWARE = [
